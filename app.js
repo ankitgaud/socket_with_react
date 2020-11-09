96dd466dacda8408a9b63f5cca561295d6ead400
Ankit Gaud <adnanXelp@bitbucket.org>
2020-11-09 14:19:24 +0530

    add

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const cors = require('cors')
+const bodyParser = require('body-parser') 
+
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(index);
 
 const server = http.createServer(app);
